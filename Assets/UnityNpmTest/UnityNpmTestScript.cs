e90df4b9a6aba8947742a61536492613ae92d397
Yaniv Gal <yanivgal123@gmail.com>
2016-05-25 21:19:43 +0300

    Added test script

---
+++ Assets/UnityNpmTest/UnityNpmTestScript.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnityNpmTestScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Nice");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
